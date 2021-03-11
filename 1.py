6a44f5d8cdeffa40e68270c225d310187f42f03e
kbuaaaaaa <72684195+kbuaaaaaa@users.noreply.github.com>
2021-03-11 18:04:37 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -34,12 +34,12 @@ class Simple_drawing_window2(QWidget):
         p.begin(self)
 
         p.setPen(QColor(0,0,0))
-        p.setBrush(QColor(0,127,0))
-        p.drawPolygon([QPoint(40,100),QPoint(180,110),QPoint(166,100),QPoint(170,105)])
-        p.setPen(QColor(278,100,0))
-        p.setBrush(QColor(255,27,0))
+        p.setBrush(QColor(255,0,0))
+        p.drawPolygon([QPoint(70,10),QPoint(120,110),QPoint(30,100),QPoint(100,50)])
+        p.setPen(QColor(255,0,0))
+        p.setBrush(QColor(255,0,0))
         p.drawPie(50,150,100,100,0,180*16)
-        p.drawPolygon([QPoint(50,250),QPoint(150,206),QPoint(180,400),])
+        p.drawPolygon([QPoint(50,20),QPoint(550,200),QPoint(170,400),])
         p.drawPixmap(QRect(200,100,320,320),self.rabbit)
         p.end()
 

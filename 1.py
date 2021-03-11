e799a280eae55202072bed62a3a6048ddbf4dfad
hbeat <49186507+hbeat@users.noreply.github.com>
2021-03-11 18:08:36 +0700

    Update 1.py
    HB window3

--- 1.py
+++ 1.py
@@ -42,13 +42,34 @@ class Simple_drawing_window2(QWidget):
         p.drawPolygon([QPoint(50,20),QPoint(550,200),QPoint(170,400),])
         p.drawPixmap(QRect(200,100,320,320),self.rabbit)
         p.end()
+        
+class Simple_drawing_window3(QWidget):
+    def __init__(self):
+       QWidget.__init__(self, None)
+       self.setWindowTitle("Simple Drawing")
+       self.rabbit = QPixmap("images/rabbit.png")
+        
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
 
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon([QPoint(70,100),QPoint(100,110),QPoint(130,100),QPoint(100,150)])
+        p.setPen(QColor(255,127,0))
+        p.setBrush(QColor(255,127,0))
+        p.drawPie(50,150,100,100,0,180*32)
+        p.drawPixmap(QRect(200,100,320,320),self.rabbit)
+        p.end()
+        
 def main():
     app = QApplication(sys.argv)
     w = Simple_drawing_window()
     w.show()
     w2 = Simple_drawing_window2()
     w2.show()
+    w3 = Simple_drawing_window3()
+    w3.show()
     return app.exec_()
 
 if __name__ == "__main__":

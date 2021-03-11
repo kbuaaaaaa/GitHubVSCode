b0d58d30e3291ae6e29e11e08af2ab53f5bb673f
Rakyun <73707080+Rakyun@users.noreply.github.com>
2021-03-11 17:56:11 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -22,6 +22,26 @@ class Simple_drawing_window(QWidget):
         p.drawPolygon([QPoint(50,200),QPoint(150,200),QPoint(100,400),])
         p.drawPixmap(QRect(200,100,320,320),self.rabbit)
         p.end()
+        
+class Simple_drawing_window2(QWidget):
+    def __init__(self):
+       QWidget.__init__(self, None)
+       self.setWindowTitle("Simple Drawing")
+       self.rabbit = QPixmap("images/rabbit.png")
+        
+    def paintEvent(self,e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0,0,0))
+        p.setBrush(QColor(0,127,0))
+        p.drawPolygon([QPoint(40,100),QPoint(180,110),QPoint(166,100),QPoint(170,105)])
+        p.setPen(QColor(278,100,0))
+        p.setBrush(QColor(255,27,0))
+        p.drawPie(50,150,100,100,0,180*16)
+        p.drawPolygon([QPoint(50,250),QPoint(150,206),QPoint(180,400),])
+        p.drawPixmap(QRect(200,100,320,320),self.rabbit)
+        p.end()
 
 def main():
     app = QApplication(sys.argv)
